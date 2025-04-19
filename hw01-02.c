e57194100da8872a3333c21497ac294d10a43de2
りん <r1nion353@gmail.com>
2025-04-19 13:08:23 +0900

    modified:   hw01-01.c modified:   hw01-02.c

--- hw01-02.c
+++ hw01-02.c
@@ -2,9 +2,12 @@
 
 int main() {
     double r1, r2;
+
     printf("抵抗値を入力: r1 r2\n");
     scanf("%lf %lf", &r1, &r2);
+
     printf("直列(Ω): %.1lf\n", r1+r2);
     printf("並列(Ω): %.1lf\n", 1.0/(1.0/r1+1.0/r2));
+    
     return 0;
 }
